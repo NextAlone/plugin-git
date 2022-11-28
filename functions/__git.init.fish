9a97c27f7c89c2d5867feea20801da46dc4df984
Next Alone <12210746+NextAlone@users.noreply.github.com>
2022-11-29 01:31:43 +0800

    Update
    Signed-off-by: Next Alone <12210746+NextAlone@users.noreply.github.com>

--- functions/__git.init.fish
+++ functions/__git.init.fish
@@ -115,6 +115,7 @@ function __git.init
     # git push abbr
     __git.create_abbr gp git push
     __git.create_abbr gpo git push origin
+    __git.create_abbr gpf git push fork
     __git.create_abbr gpu git push --set-upstream origin 
     __git.create_abbr gp! git push --force-with-lease
 
